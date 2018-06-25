3410a2ead4b36114b1517589d14cda1ecb038c78
madseif <mseif15@berkeley.edu>
2018-06-24 23:32:44 -0700

    test

---
+++ PyBank/main.py
@@ -0,0 +1,24 @@
+import os
+import csv
+
+# Path to collect data from the Resources folder
+budgetCSV = os.path.join('budget_data.csv')
+
+with open(budgetCSV, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    csv_header = next(csvreader)
+
+    monthcount = 0
+    netprofitloss = 0
+
+    for row in csvreader:
+        monthcount = monthcount +1
+        netprofitloss += int(row[1])
+    print("Total Months: " + str(monthcount))
+    print("Total: $" + str(netprofitloss))
+
+    
+  
+    
+
