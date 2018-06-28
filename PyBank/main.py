b639e98d537e9def69751a128e6d530aa8933d13
madseif <mseif15@berkeley.edu>
2018-06-27 23:13:30 -0700

    test

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,23 +9,36 @@ with open(budgetCSV, newline="") as csvfile:
 
     csv_header = next(csvreader)
 
+   
+
     monthcount = 0
     netprofitloss = 0
     increase = 0
     decrease = 0
+    rowints = []
+    months = []
 
     for row in csvreader:
         monthcount = monthcount +1
         netprofitloss += int(row[1])
-        #increase = max(float(row[1])
+        rowint = int(row[1])
+        rowints.append(rowint)
+        months.append(row[0])
         
+      
     print("Total Months: " + str(monthcount))
     print("Total: $" + str(netprofitloss))
     print("Average Change: $" + str(round((netprofitloss/monthcount), 2)))
-    print(str(increase))
-    print(row)
 
+    profitlist = zip(months, rowints)
+    listlength = len(profitlist)
+    for i in range(listlength):
+        maximum = max(profitlist)
+        print("Greatest Increase in Profits: %s ($%r)" % (maximum))
+
+        minimum = min(profitlist)
+        print("Greatest Decrease in Profits: %s ($%r)" % (minimum))
     
   
-    
+   
 
